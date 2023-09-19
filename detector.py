6b2a2db660d2721e22ca64be0fccbec69ff7e47f
melissaoli07 <melissaoliveiraa11@gmail.com>
2023-09-19 17:53:32 -0300

    COMMIT

--- detector.py
+++ detector.py
@@ -2,4 +2,20 @@ from ultralytics import YOLO
 
 modelo = YOLO('yolov8n.pt')
 
-modelo.predict(source='0', show=True)
+#modelo.predict(source='0', show=True)
+
+
+# Realiza a detecção de objetos na webcam
+resultado = modelo.predict(source='0')
+
+# Obtém as previsões
+previsoes = resultado.pandas().xyxy[0]
+
+# Verifica se a classe "bicicleta" está presente nas previsões
+bicicleta_presente = any(previsoes['name'] == 'bicycle')
+
+if bicicleta_presente:
+    print('Bicicleta detectada!')
+else:
+    print('Nenhuma bicicleta detectada.')
+
